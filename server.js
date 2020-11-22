4ebe80a12778b1897ae82633ea7833f608c7d477
Amee Li <ameejli@gmail.com>
2020-11-21 17:47:02 -0800

    Completed function to fetch all metrics

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const fetch = require("node-fetch");
 
 const base_url = "https://interview-api.sbly.com/ad-insights?";
 
-let usp = new URLSearchParams(
-  "metrics=spend,revenue,impressions,clicks&accessToken=SHAREABLY_SECRET_TOKEN"
-);
-
 let dates = [
   "2020-01-01",
   "2020-01-02",
@@ -18,14 +14,16 @@ let dates = [
   "2020-01-05",
 ];
 
-function createUrls() {
-  let urls = [];
+function createParams() {
+  let params = [];
   for (date in dates) {
+    let usp = new URLSearchParams(
+      "metrics=spend,revenue,impressions,clicks&accessToken=SHAREABLY_SECRET_TOKEN"
+    );
     usp.set("date", dates[date]);
-    url = base_url.concat(usp.toString());
-    urls.push(url);
+    params.push(usp);
   }
-  return urls;
+  return params;
 }
 
 async function fetchAdMetrics(url) {
@@ -38,20 +36,45 @@ async function fetchAdMetrics(url) {
   }
 }
 
-const urls = createUrls();
-
+const params = createParams();
 async function buildInsights() {
-  insights = [];
-  for (url in urls) {
+  ad_insights = {};
+  for (i in params) {
+    const date = params[i].get("date").slice([-1]);
+    const url = base_url.concat(params[i].toString());
+
     try {
-      const insight = await fetchAdMetrics(urls[url]);
-      // processInsight(insight)
-      insights.push(insight);
+      const insights = await fetchAdMetrics(url);
+
+      for (i in insights) {
+        if (!(insights[i].id in ad_insights)) {
+          ad_insights[insights[i].id] = {};
+        }
+
+        ad_insights[insights[i].id][date] = insights[i];
+      }
     } catch (err) {
       console.log(err);
     }
   }
-  return insights;
+  return ad_insights;
 }
 
 buildInsights().then((insights) => console.log(insights));
+
+//   "ad_id": {
+//     "weighted_average_profit_margin": "int",
+//     "curr_budget": "int",
+//     "next_budget": "int",
+//     "most_recent_perf": "int representing date";
+//     "most_recent_profit_margin": "int";
+//     "weights_total": "int",
+//     "date1": {
+//       "spend": "",
+//       "rev": "",
+//       "impressions": "",
+//       "clicks": "",
+//       "profit_margin": "calculate int, (rev - spend) / spend"
+//     }
+//   }
+// }
