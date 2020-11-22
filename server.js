1c88f771c7bb6023dfc508e7db3dd8b74cc7d2bf
Amee Li <ameejli@gmail.com>
2020-11-21 16:48:12 -0800

    Build insights for all dates

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+const fetch = require("node-fetch");
 
-app.get("/api/customers", (req, res) => {
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
-  ];
+// const app = express();
 
-  res.json(customers);
-});
+const base_url = "https://interview-api.sbly.com/ad-insights?";
 
-const port = 5000;
+let usp = new URLSearchParams(
+  "metrics=spend,revenue,impressions,clicks&accessToken=SHAREABLY_SECRET_TOKEN"
+);
 
-app.listen(port, () => `Server running on port ${port}`);
+let dates = [
+  "2020-01-01",
+  "2020-01-02",
+  "2020-01-03",
+  "2020-01-04",
+  "2020-01-05",
+];
+
+function createUrls() {
+  let urls = [];
+  for (date in dates) {
+    usp.set("date", dates[date]);
+    url = base_url.concat(usp.toString());
+    urls.push(url);
+  }
+  return urls;
+}
+
+async function fetchAdMetrics(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const urls = createUrls();
+
+async function buildInsights() {
+  insights = [];
+  for (url in urls) {
+    try {
+      const insight = await fetchAdMetrics(urls[url]);
+      // processInsight(insight)
+      insights.push(insight);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return insights;
+}
+
+buildInsights().then((insights) => console.log(insights));
